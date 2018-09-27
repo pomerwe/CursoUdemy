5df2f8ce1aac80c0f1b0f0b7415a30f640b6003a
Estevão Alves <pomerwe@gmail.com>
2018-09-27 11:52:26 -0300

    Projeto criado

---
+++ src/main/java/br/com/udemycurso/domain/CursoUdemyApplication.java
@@ -0,0 +1,12 @@
+package br.com.udemycurso.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+}
