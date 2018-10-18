2759c281578a15bd4c28fc9ce8642cd4be4f9982
Estevão Alves <pomerwe@gmail.com>
2018-10-18 12:01:42 -0300

    Cliente só poderá buscar os próprios pedidos

--- src/main/java/br/com/udemycurso/resources/PedidoResource.java
+++ src/main/java/br/com/udemycurso/resources/PedidoResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +14,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.udemycurso.domain.Cliente;
 import br.com.udemycurso.domain.Pedido;
+import br.com.udemycurso.domain.Produto;
+import br.com.udemycurso.dto.ProdutoDTO;
+import br.com.udemycurso.resources.utils.URL;
 import br.com.udemycurso.services.PedidoService;
 
 @RestController
@@ -27,12 +32,12 @@ public class PedidoResource {
 	@Autowired
 	private PedidoService pedServ;
 	
-	@GetMapping
-	public ResponseEntity<List<Pedido>> listar() {
-		List<Pedido> categorias = pedServ.listar();
-		return ResponseEntity.status(HttpStatus.OK).body(categorias);
-		
-	}
+//	@GetMapping
+//	public ResponseEntity<List<Pedido>> listar() {
+//		List<Pedido> categorias = pedServ.listar();
+//		return ResponseEntity.status(HttpStatus.OK).body(categorias);
+//		
+//	}
 	@GetMapping("/{id}")
 	public ResponseEntity<Pedido> buscarPedido(@PathVariable("id") Long id){
 		Pedido categoria = pedServ.buscar(id);
@@ -50,6 +55,20 @@ public class PedidoResource {
 				buildAndExpand(obj.getId()).
 				toUri();
 		return ResponseEntity.created(uri).build();
+		
+	}
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> listarPages(			
+		    @RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="categoria")String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC")String direction
+			) {
+		
+	
+		Page<Pedido> list = pedServ.pesquisar(page, linesPerPage, orderBy, direction);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(list);
 		
 	}
 	
--- src/main/java/br/com/udemycurso/services/PedidoService.java
+++ src/main/java/br/com/udemycurso/services/PedidoService.java
@@ -5,20 +5,31 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.udemycurso.domain.Cliente;
 import br.com.udemycurso.domain.ItemPedido;
 import br.com.udemycurso.domain.PagamentoComBoleto;
 import br.com.udemycurso.domain.Pedido;
+import br.com.udemycurso.domain.Produto;
 import br.com.udemycurso.domain.enums.EstadoPagamento;
+import br.com.udemycurso.domain.enums.Perfil;
 import br.com.udemycurso.repositories.ItemPedidoRepository;
 import br.com.udemycurso.repositories.PagamentoRepository;
 import br.com.udemycurso.repositories.PedidoRepository;
+import br.com.udemycurso.security.UserSS;
+import br.com.udemycurso.services.exceptions.AuthorizationException;
 import br.com.udemycurso.services.exceptions.NotFoundException;
 
 @Service
 public class PedidoService {
 
+	@Autowired
+	private ClienteService cliServ;
+	
 	@Autowired
 	private PedidoRepository repo;
 	
@@ -41,7 +52,15 @@ public List<Pedido> listar(){
 	}
 	
 	public Pedido buscar(Long id){
-		Pedido x = validation(id);	
+		
+		UserSS user = UserService.authenticatedUser();
+		Pedido x = validation(id);
+		
+		if(user==null || !user.hasRole(Perfil.ADMIN) && user.getId()!= x.getCliente().getId()) {
+			throw new AuthorizationException("Acesso negado");			
+		}
+		
+			
 		return x ;
 	}
      
@@ -73,6 +92,21 @@ public List<Pedido> listar(){
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
 		return obj;
+	}
+	
+   public Page<Pedido> pesquisar(Integer page, Integer linesPerPage, String orderBy,String direction){
+		
+	  
+	    UserSS user = UserService.authenticatedUser();
+	    Cliente cli = cliServ.buscar(user.getId());		
+		if(user==null || !user.hasRole(Perfil.ADMIN)) {
+			throw new AuthorizationException("Acesso negado");			
+		}
+		@SuppressWarnings("deprecation")
+		PageRequest pageRequest= new PageRequest(page,linesPerPage , Direction.valueOf(direction),orderBy);
+		Page<Pedido> pedido = repo.findByCliente(cli, pageRequest);
+		return pedido;
+		
 	}
 	
 }
